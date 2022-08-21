1daa48e1e59b5cb39aa88577a9a0067727350f89
Krizia <kolitoquit@luc.edu>
2022-08-20 22:50:04 -0500

    updated the price variable to 33

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        double price = 23.00;
+        double price = 33.00;
 	    double discount = .10;
 	   price = (1 - discount) * price;
 	   System.out.println("With discount the price is " +  price);
